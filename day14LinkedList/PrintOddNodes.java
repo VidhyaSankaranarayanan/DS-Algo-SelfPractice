2ab71754258dfe259dac839cc42eb1e660439a1b
VidhyaSankaranarayanan <86971707+VidhyaSankaranarayanan@users.noreply.github.com>
2021-07-24 21:39:41 +0530

    Add files via upload

---
+++ day14LinkedList/PrintOddNodes.java
@@ -0,0 +1,92 @@
+package week4day1;
+
+import org.junit.Test;
+
+public class PrintOddNodes {
+	public class Node{
+		int value;
+		Node next;
+		Node(int value){
+			this.value = value;
+			next = null;
+		}
+		
+		Node(){
+			//this.value = value;
+			next = null;
+		}
+	}
+	public Node addNode(int key) {
+		return new Node(key);
+	}
+	public void printAllNodes(Node node) {
+		while(node != null) {
+			System.out.println(node.value);
+			node = node.next;
+		}
+	}
+	
+	public int countNodes(Node node) {
+		int count = 1;
+		while(node!=null) {
+			if(node.next!=null)
+				count++;
+			node=node.next;
+		}
+		return count;
+	}
+	
+	public Node printOddNodes(Node node, int length) {
+		Node oddNodes = new Node();
+		Node current = oddNodes;
+		while(node!=null) {
+			current.next = node;
+			if(node.next!=null)
+				node = node.next.next;
+			else
+				node = node.next;
+			current = current.next;
+		}
+		if(length%2==0)
+			current.next=null;
+		return oddNodes.next;
+		
+	}
+	
+	public Node printOddNodesUsingRecursion(Node node) {
+		
+		if(node==null)
+			return null;
+		
+		return	node.next = printOddNodesUsingRecursion(node.next.next);
+		
+		
+	}
+	
+	public Node reverseNodes(Node node) {
+		Node current = node, prev = null, next = null;
+		while(current!=null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;			
+		}
+		return prev;
+	}
+	
+	@Test
+	 public void test() {
+        Node head1 = addNode(1);
+        head1.next = addNode(2);
+        head1.next.next = addNode(3);
+        head1.next.next.next = addNode(4);
+        head1.next.next.next.next = addNode(5);
+        head1.next.next.next.next.next = addNode(6);
+        head1.next.next.next.next.next.next = addNode(7);
+        int length = countNodes(head1);
+        Node node = printOddNodes(head1, length);
+        Node reverseNode = reverseNodes(head1);
+//        printAllNodes(node);
+        printAllNodes(reverseNode);
+	}
+}
