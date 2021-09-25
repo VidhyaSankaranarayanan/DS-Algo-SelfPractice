4d8efb3ba1a1058d45e3483b79154280e1501489
Vidhya S <86971707+VidhyaSankaranarayanan@users.noreply.github.com>
2021-09-25 15:16:40 +0530

    Update program.java

--- jenkinsPractice/program.java
+++ jenkinsPractice/program.java
@@ -5,7 +5,7 @@ public class program {
 	public static void main(String[] args) {
 		System.out.println("SCM trigger");
 		System.out.println("SCM trigger");
-
+System.out.println("Webhook trigger");
 	}
 
 }
